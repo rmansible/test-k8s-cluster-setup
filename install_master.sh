1c8303de9b5809e7e5fe51c8c71e0022280570bf
rmansible <rm.ansible@gmail.com>
2025-11-13 11:34:20 +0530

    Create install_master.sh

---
+++ install_master.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo kubeadm init --config=kubeadm-config.yaml
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
